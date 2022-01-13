2232a2b7f54e9ae83d478ba63ab906f006c28653
pauls <sargespaul@gmail.com>
2022-01-13 17:39:27 +0100

    Init Projet Hopital

---
+++ src/dao/IDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.util.List;
+
+public interface IDAO<T,K> {
+	
+	String urlBdd = "jdbc:mysql://localhost:3306/safarimeetic?characterEncoding=UTF-8";
+	String loginBdd = "root";
+	String passwordBdd = "";
+	
+	
+	public T findById(K id);
+	public List<T> findAll();
+	public void insert(T o);
+	public void update(T o);
+	public void delete(K id);
+	
+
+}
