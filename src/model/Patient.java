cff607d9491eb0c66ab5d83c3252d32ef42db16d
Anthony TROCELLIER <anthony.trocellier@free.fr>
2022-01-14 11:51:06 +0100

    Make Patient class serializable

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,66 +1,68 @@
 package model;
 
-public class Patient {
-		
-		private Integer id;
-		private String nom;
-		private String prenom;
-		
-		public Patient(String nom, String prenom) {
-			this.nom = nom;
-			this.prenom = prenom;
-		}
-		
-		public Patient(Integer id, String nom, String prenom) {
-			this.id = id;
-			this.nom = nom;
-			this.prenom = prenom;
-		}
+import java.io.Serializable;
 
-		public Integer getId() {
-			return id;
-		}
+public class Patient implements Serializable {
 
+	/**
+	 * Generated serial version UID
+	 */
+	private static final long serialVersionUID = -1916016033528765669L;
 
+	private Integer id;
+	private String nom;
+	private String prenom;
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
+	public Patient(String nom, String prenom) {
+		this.nom = nom;
+		this.prenom = prenom;
+	}
 
+	public Patient(Integer id, String nom, String prenom) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
 
-
-		public String getNom() {
-			return nom;
-		}
-
-
-		public void setNom(String nom) {
-			this.nom = nom;
-		}
+	public Integer getId() {
+		return id;
+	}
 
 
 
-		public String getPrenom() {
-			return prenom;
-		}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 
 
-		public void setPrenom(String prenom) {
-			this.prenom = prenom;
-		}
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
 
 
-		@Override
-		public String toString() {
-			return "Patient [nom=" + nom + ", prenom=" + prenom + "]";
-		}
+	public String getPrenom() {
+		return prenom;
+	}
 
 
 
-		
-		
-	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Patient [nom=" + nom + ", prenom=" + prenom + "]";
+	}
+
 
 }
